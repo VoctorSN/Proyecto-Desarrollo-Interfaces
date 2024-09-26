5983c5358c69344ba4de12fd7a576a79b80bbbd8
Victor <victorsancheznogueira@gmail.com>
2024-09-26 08:20:37 +0200

    25/09/24

---
+++ main.py
@@ -0,0 +1,16 @@
+from venPrincipal import *
+import sys
+import var
+
+class Main(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        var.ui = Ui_venPrincipal()
+        var.ui.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = Main()
+    window.showMaximized()
+    sys.exit(app.exec())
